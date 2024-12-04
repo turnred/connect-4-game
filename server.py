1f121e5d08b919081786ca41dbf4c79e8a0e0773
turnred <112022897+turnred@users.noreply.github.com>
2024-12-04 13:39:08 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,61 +1,69 @@
 import sys
 import socket
 import selectors
-import types
 from libserver.message import *
+from libserver.messages import *
 
-sel = selectors.DefaultSelector()
-MAX_CONNECTIONS = 2
-num_connected = 0
+class Server:
+    def __init__(self, port: int) -> None:
+        self.port = port
+        self.sel = selectors.DefaultSelector()
+        self.connected = {}
+        self.message = Message(self.sel, self.connected)
+        self.messages = Messages()
 
-def build_server(host, port):
-    lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    def build_server(self):
+        """Establishes a listening socket"""
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        try:
+            self.sock.bind(('', self.port))
+        except Exception:
+            print("Fatal Error: Cannot bind port given")
+        self.sock.listen()
+        print("Server listening on port", port)
+        
+    def build_connection(self, sock):
+        """Called when a client connects for the first time"""
+        connection, address = sock.accept()
+        print("Recieved connection from client", address)
+        if len(self.connected) >= 2:
+            print("Server can only support 2 clients, rejecting client...")
+            connection.sendall(self.messages.msg_refuseconn("playerlimit"))
+            connection.shutdown()
+            return
+        self.connected[connection] = address
+        self.message.create_user(address)
+        self.sel.register(connection, selectors.EVENT_READ | selectors.EVENT_WRITE)
+        connection.sendall(self.messages.msg_acceptconn())
+
+    def close_connection(self, sock):
+        """Closes the client's lingering sockets when the server connection is dropped"""
+        address = self.connected.pop(sock)
+        self.sel.unregister(sock)
+        print(f'Connection lost to client at{address}, removing client.')
+
+    def listen(self):
+        """Main listening loop"""
+        self.build_server()
+        while True:
+            for key, _ in self.sel.select():
+                sock = key.fileobj
+                if sock is self.sock:
+                    self.build_connection(sock)
+                else:
+                    if key.events & selectors.EVENT_READ:
+                        self.message.read(sock)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: server.py -p [port]")
+        sys.exit(1)
+    port = int(sys.argv[2])
+    server = Server(port)
     try:
-        lsock.bind((host, port))
-    except Exception:
-        print("server.py: Fatal Exception: Port Can't Be Bound")
-    lsock.listen()
-    lsock.setblocking(False)
-    print("server.py: Server started on ", (host, port))
-    sel.register(lsock, selectors.EVENT_READ, data=None)
-    
-def build_connection(sock):
-    connection, address = sock.accept()
-    print("server.py: Recieved connection from client", connection, address)
-    if num_connected >= 2:
-        print("server.py: Server can only support 2 clients, rejecting client...")
-        connection.sendall(messages.conn_close())
-        connection.close()
-        return
-    num_connected += 1
-    connection.setblocking(False)
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(connection, events, data=Message(sel, connection, address))
-
-def get_num_connections():
-    return num_connected
-
-if len(sys.argv) != 3:
-    print("Usage: server.py -p [PORT]")
-    sys.exit(1)
-host = '0.0.0.0'
-port = int(sys.argv[2])
-build_server(host, port)
-try:
-    while True:
-        events = sel.select(timeout=None)
-        for key, mask in events:
-            if key.data is None:
-                build_connection(key.fileobj)
-            else:
-                try:
-                    key.data.process_events(mask)
-                except Exception:
-                    print("server.py: Error handling connection, assuming client has disconnected.")
-                    key.data.close()
-                    
-except KeyboardInterrupt:
-    print("Gracefully exiting, keyboard interrupt")
-finally:
-    sel.close()
+        server.listen() 
+    except KeyboardInterrupt:
+        print("Gracefully exiting, keyboard interrupt")
+    finally:
+        server.sel.close()
