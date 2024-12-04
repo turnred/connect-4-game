f3b31a330f43abc50b968cfdd15d47f1b8907a06
turnred <112022897+turnred@users.noreply.github.com>
2024-12-04 13:40:38 -0700

    Update messages.py

--- libserver/messages.py
+++ libserver/messages.py
@@ -1,43 +1,66 @@
 import struct
 import json
 
-def _pack(message):
-    bytes_msg = _json_encode(message, "utf-8")
-    return struct.pack(f'<i{len(bytes_msg)}s', len(bytes_msg), bytes_msg)
+class Messages:
+    def _pack(message):
+        json_msg = json.dumps(message).encode("utf-8")
+        return struct.pack(f'<I{len(json_msg)}s', len(json_msg), json_msg)
 
-def _json_encode(self, obj, encoding):
-    return json.dumps(obj, ensure_ascii=False).encode(encoding)
-
-def conn_refuse():
-    message = {
-        "message": "refused"
-    }
-    return _pack(message)
-
-def conn_wait():
-    message = {
-        "message": "wait"
-    }
-    return _pack(message)
-
-def move():
-    #TODO: game logic to check for valid move
-    if (True):
+    def msg_gamestatus(self, current_turn, turn_count, previous_move):
         message = {
-            "message": "accepted"
+            "type": "gamestatus",
+            "current_turn": current_turn,
+            "turn_count": turn_count,
+            "prevmove": previous_move
         }
-        return _pack(message)
-    
-def board():
-    #TODO: needs a board to return
-    message = {
-        "message": "board"
-    }
-    return _pack(message)
+        return self._pack(message)
 
-def end_game():
-    #TODO: update to support a winner
-    message = {
-        "message": "gameover"
-    }
-    return _pack(message)
+    def msg_gamestate(self, gamestate):
+        message = {
+            "type": "gamestate",
+            "state": gamestate
+        }
+        return self._pack(message)
+    
+    def msg_gameover(self, winner):
+        message = {
+            "type": "gamestate",
+            "state": "over",
+            "winner": winner
+        }
+        return self._pack(message)
+
+    def msg_move(self, valid_move):
+        message = {
+            "type": "moveresponse",
+            "move": valid_move
+        }
+        return self._pack(message)
+
+    def msg_acceptconn(self):
+        message = {
+            "type": "connresponse",
+            "status": "accepted"
+        }
+        return self._pack(message)
+
+    def msg_refuseconn(self, reason):
+        message = {
+            "type": "connresponse",
+            "status": "refused",
+            "reason": reason
+        }
+        return self._pack(message)
+    
+    def msg_nameaccept(self):
+        message = {
+            "type": "nameresponse",
+            "status": "accepted"
+        }
+        return self._pack(message)
+    
+    def msg_namerefuse(self):
+        message = {
+            "type": "nameresponse",
+            "status": "refused"
+        }
